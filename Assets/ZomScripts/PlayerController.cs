7197a2b01306d254c172b4623fc3130a4a2ee20f
zomawia.sailo <zomawia.sailo@SEA-8E3F0D>
2017-05-03 12:51:10 -0700

    2nd

--- Assets/ZomScripts/PlayerController.cs
+++ Assets/ZomScripts/PlayerController.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿// JunkBot PlayerController
+// by Zomawia Sailo
+
+// Holds player states, fighting system state machine
+// Hooks up with an Animator state machine to sync states, actions with animations
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,26 +18,76 @@ public class PlayerController : MonoBehaviour {
         BLOCK
     }
 
+    enum AttackState
+    {
+        IDLE,
+        WINDUP,
+        RELEASE,
+        RECOVERY
+    }
+
     float leanAxis = 0f;
+    float powerPunchTimer = 0f;
+    float maxPowerPunch = 1.25f;
+
+    float animPlayTimer = 0f;
 
     PlayerState myState;
+    AttackState myAttackState;
+
     Animator anim;
+    Animation myAnimation;
     AnimatorStateInfo animState;
     AnimatorClipInfo[] animClip;
 
 	// Use this for initialization
 	void Start () {
+
         myState = PlayerState.IDLE;
-        anim = GetComponent<Animator>();
+
+        if (anim != null)
+        {
+            anim = GetComponent<Animator>();
+            animState = anim.GetCurrentAnimatorStateInfo(0);
+            animClip = anim.GetCurrentAnimatorClipInfo(0);
+            myAnimation = GetComponent<Animation>();
+        }
+
 	}
 	
+    //Hit detection
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Hand")
+        {
+            if (myAttackState == AttackState.WINDUP && myState == PlayerState.ATTACK)
+            {
+                //do flinch
+                
+                //stop current animation
+            }
+        }
+    }
+
     void DoPunch(string stateName)
     {
         //windup
-        anim.Play(stateName);
-        //release
+        anim.Play(stateName, 0, 0f);
+        myAttackState = AttackState.WINDUP;
+        
+        if ((animClip[0].clip.length % 1 ) >= .25f && myAttackState == AttackState.WINDUP)
+        {
+            //release
+            anim.Play(stateName, 0, .25f);
+            myAttackState = AttackState.RELEASE;
+        }
 
-        //recovery
+        if ((animClip[0].clip.length % 1) >= .75f && myAttackState == AttackState.RELEASE)
+        {
+            //recovery
+            anim.Play(stateName, 0, .75f);
+            myAttackState = AttackState.RECOVERY;
+        }                    
     }
 
     void HandleInput()
@@ -39,20 +95,55 @@ public class PlayerController : MonoBehaviour {
         leanAxis = Input.GetAxis("Horizontal") * Time.deltaTime;
         // hook up leanAxis to lean animation
         anim.SetFloat("Lean", leanAxis);
-
-        //Punch from Idle
+                
         if (myState == PlayerState.IDLE)
         {
-            if (Input.GetKeyDown("RightPunchHigh"))
+            // idle to punch
+            if (Input.GetKeyDown("RightPunchHigh") || Input.GetKeyDown("RightPunchLow") || 
+                Input.GetKeyDown("LeftPunchHigh") || Input.GetKeyDown("LeftPunchLow") )
             {
-                //jab
-                if (Input.GetKeyUp("RightPunchHigh"))
-                {
-                    DoPunch("RightPunchHigh");
-                }
+                myState = PlayerState.ATTACK;
             }
         }
+
+        else if (myState == PlayerState.ATTACK)
+        {
+            // decide to throw normal punch or charge up
+            if (Input.GetKeyUp("RightPunchHigh"))
+            {
+                DoPunch("RightPunchHigh");
+            }
+            else if (Input.GetKeyUp("RightPunchLow"))
+            {
+                DoPunch("RightPunchLow");
+            }
+            else if (Input.GetKeyUp("LeftPunchHigh"))
+            {
+                DoPunch("LeftPunchHigh");
+            }
+            else if (Input.GetKeyUp("LeftPunchLow"))
+            {
+                DoPunch("LeftPunchLow");
+            }
+
+            //we can go back to idle or start combo
+        }     
         
+        else if (myState == PlayerState.COMBO)
+        {
+
+        }
+        
+        else if (myState == PlayerState.BLOCK)
+        {
+            if (Input.GetKeyDown("RightPunchHigh") || Input.GetKeyDown("RightPunchLow") ||
+                    Input.GetKeyDown("LeftPunchHigh") || Input.GetKeyDown("LeftPunchLow"))
+            {
+                //do nothing
+            }
+
+
+        }
 
     }
 
